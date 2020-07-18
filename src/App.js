84222ec7889223927f4b1ae6a92d4b091507c590
keys8410 <keys8410@gmail.com>
2020-07-18 15:44:47 -0300

    rotas configuradas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-    return <h1>Where's my diploma?</h1>;
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/sign-in'>Sign in</Link>
+            </li>
+
+            <li>
+              <Link to='/sign-up'>Sign up</Link>
+            </li>
+
+            <li>
+              <Link to='/manage/links/create'>Create links</Link>
+            </li>
+
+            <li>
+              <Link to='/manage/links/edit'>Edit links</Link>
+            </li>
+
+            <li>
+              <Link to='/manage/links'>Links</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/sign-in'>Sign in</Route>
+          <Route path='/sign-up'>Sign up</Route>
+          <Route path='/manage/links/create'>Create links</Route>
+          <Route path='/manage/links/edit'>Edit links</Route>
+          <Route path='/manage/links'>Links</Route>
+          <Route path='/'>
+            <h1>home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
